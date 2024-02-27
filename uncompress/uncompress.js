93764f4974bae60c2dffe1bc2b33d3240432546f
Sean Hoang <116997524+SonQHoang@users.noreply.github.com>
2024-02-27 15:52:31 -0800

    Solved 'uncompress' (.js)

--- uncompress/uncompress.js
+++ uncompress/uncompress.js
@@ -1,22 +1,21 @@
 const uncompress = (s) => {
-  // todo
+  const numbers = "0123456789";
   let result = [];
   let i = 0;
   let j = 0;
-  let numbers = "0123456789"
   
   while(j < s.length) {
     if(numbers.includes(s[j])) {
-      j++;
+      j += 1;
     } else {
-      const num = Number(s.slice(i, j)); // Gives me my number and casts it as a number;
+      const num = Number(s.slice(i, j))
       for(let count = 0; count < num; count++) {
         result.push(s[j])
       }
       j++;
       i = j;
     }
-  } 
+  }
   return result.join("");
 };
 ​
@@ -24,11 +23,5 @@ module.exports = {
   uncompress,
 };
 ​
-//  So we need to run through the entire string and uncompress it
-// Define 2 pointers (I & J) to track position
-// define result array
-// Define numbers to check against=
-// We'll use J to run through the string til we reach the end
-// As we increment J
-  // If we hit a number we'll increment til we hit a letter
-  
+// 2 pointer method
+​
