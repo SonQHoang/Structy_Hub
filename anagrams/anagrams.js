3a1595757874ddb76fbb56cf34b69736dcc6e028
Sean Hoang <116997524+SonQHoang@users.noreply.github.com>
2024-02-28 15:12:07 -0800

    Solved 'anagrams' (.js)

--- anagrams/anagrams.js
+++ anagrams/anagrams.js
@@ -1,26 +1,23 @@
+
 const anagrams = (s1, s2) => {
-  if(s1.length !== s2.length) return false;
+  const charMap = {};
   
-  const charCount = {};
-  
-  for (let char of s1) {
-    if (!(char in charCount)) {
-      charCount[char] = 0;
+  for(let char of s1) {
+    if(!(charMap[char])) {
+      charMap[char] = 0;
     }
-    charCount[char] += 1;
+    charMap[char] += 1;
   }
-  // console.log(charCount);
-  
-  for (let char of s2) {
-    if(char in charCount){
-        charCount[char] -= 1;
+  // console.log(charMap)
+  for(let char of s2) {
+    if(charMap[char]) {
+      charMap[char] -= 1;
     } else {
-      return false; // You've found a char that's not in S1, but IS in s2 so not an anagram
+      return false;
     }
   }
-  
-  for(let char in charCount) { // in loop of an object iterates THROUGH THE KEYS of that object
-    if(charCount[char] !== 0) {
+  for(let char in charMap) {
+    if(charMap[char] !== 0) {
       return false;
     }
   }
@@ -31,8 +28,3 @@ module.exports = {
   anagrams,
 };
 ​
-// Random Trivia: You cannot compare two objects in JS and expect to see if they're equal in terms of keys and values
-​
-// Strict equality between two objects checks if they're the same object in memory;\
-​
-  for(let char in charCount) { // in loop of an object iterates THROUGH THE KEYS of that object
