ad388778dd8247296756632a292aab6fb976a39c
Sean Hoang <116997524+SonQHoang@users.noreply.github.com>
2024-02-28 14:16:11 -0800

    Solved 'compress' (.js)

--- compress/compress.js
+++ compress/compress.js
@@ -1,22 +1,23 @@
+
 const compress = (s) => {
+  // todo
   let i = 0;
   let j = 0;
   let result = [];
   
-  while(j <= s.length){
+  while(j <= s.length) {
     if(s[i] === s[j]) {
-      j++
+      j+= 1;
     } else {
       const num = j - i;
       if(num === 1) {
-        result.push(s[i]);
+        result.push(s[i])
       } else {
         result.push(num, s[i])
       }
       i = j;
     }
   }
-  
   return result.join("");
 };
 ​
@@ -24,4 +25,15 @@ module.exports = {
   compress,
 };
 ​
-         i = j;
+// 2 pointer method again;
+// pointer i and pointer j;
+// create an array to hold the final compressed string
+// if i === j then keep moving j up
+// once i and j are different, we want to get the difference of their indexes
+// then push the number and s[i] into the result array
+// if the number === 1 then just push s[i];
+​
+//      i
+//ccaaatsss_
+//         j
+// [2c3at]
